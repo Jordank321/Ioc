36a5b6fc4931fb6412b9b24d5f4d6046533fac2c
Jordan Kelwick <jordankelwick@gmail.com>
2017-11-23 21:30:42 +0000

    Improved constructor filter and ordering

--- Ioc/Class1.cs
+++ Ioc/Class1.cs
@@ -61,15 +61,18 @@ namespace Ioc
         private object Construct(Type targetType)
         {
             // look for constructors
-            var constructors = targetType.GetConstructors();
+            var constructors = targetType.GetConstructors()
+                .Where(c => c.IsPublic)
+                .OrderBy(c => c.GetParameters().Count());
 
+            /*
             // select ones we like
             var possibleConstructors = constructors.Where(c => c.IsPublic);
 
             // order by parameter count - easiest first
             var orderedConstructors = possibleConstructors.OrderBy(c => c.GetParameters().Count());
-
-            foreach(var constructor in orderedConstructors)
+            */
+            foreach(var constructor in constructors)
             {
                 // try and resolve any parameters for the constructor
                 // using ourselves...
