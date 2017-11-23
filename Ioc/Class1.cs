57ea4ca536a42e96aca56dc9a1024ee2990f2a6a
jordan kelwick <jordan.kelwick@gilmond.com>
2017-11-23 15:22:55 +0000

    New project

---
+++ Ioc/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ioc
+{
+    public class IocContainer
+    {
+        private Dictionary<Type, Func<object>> _resolvers = new Dictionary<Type, Func<object>>();
+
+        public void Register<T>()
+        {
+            _resolvers.Add(typeof(T), ()=>Construct(typeof(T)));
+        }
+
+        public void Register<T>(Func<T> builderFunc)
+        {
+            _resolvers.Add(typeof(T), ()=>builderFunc());
+        }
+
+        public void Register<T, TConcrete>() where TConcrete : T
+        {
+            _resolvers.Add(typeof(T), () => Construct(typeof(TConcrete)));
+        }
+
+        public void Register<T>(Action<T> builderAction)
+        {
+            _resolvers.Add(typeof(T), () =>
+            {
+                var instance = Construct(typeof(T));
+                builderAction((T)instance);
+                return instance;
+            });
+        }
+
+        public T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        public object Resolve(Type t)
+        {
+            if (_resolvers.ContainsKey(t))
+            {
+                return _resolvers[t]();
+            }
+            else
+            {
+                if (!t.IsValueType) return null;
+                return Activator.CreateInstance(t);
+            }
+        }
+
+        public bool HasRegistration(Type t)
+        {
+            return _resolvers.ContainsKey(t);
+        }
+
+        private object Construct(Type targetType)
+        {
+            // look for constructors
+            var constructors = targetType.GetConstructors();
+
+            // select ones we like
+            var possibleConstructors = constructors.Where(c => c.IsPublic);
+
+            // order by parameter count - easiest first
+            var orderedConstructors = possibleConstructors.OrderBy(c => c.GetParameters().Count());
+
+            foreach(var constructor in orderedConstructors)
+            {
+                // try and resolve any parameters for the constructor
+                // using ourselves...
+                var parameters = constructor.GetParameters();
+                var parameterValues = new List<object>();
+
+                foreach(var parameter in parameters)
+                {
+                    var parameterType = parameter.ParameterType;
+                    if (!HasRegistration(parameterType))
+                    {
+                        continue;
+                    }
+                    parameterValues.Add(Resolve(parameterType));
+                }
+
+                if (parameterValues.Count() != parameters.Length)
+                {
+                    continue;
+                }
+                else
+                {
+                    return constructor.Invoke(parameterValues.ToArray());
+                }
+            }
+
+            throw new InvalidOperationException($"Unable to resolve constructor of type {targetType.FullName}");
+        }
+    }
+}
---
+++ IocTests/Class1.cs
@@ -0,0 +1,194 @@
+﻿using FluentAssertions;
+using Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IocTests
+{
+    public class IocTesting
+    {
+        [Fact]
+        public void CanCreateSimpleTypeWithoutConstructors()
+        {
+            var container = new IocContainer();
+            container.Register<MySimpleThing>();
+            var mySimpleThing = container.Resolve<MySimpleThing>();
+            mySimpleThing.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void CanCreateSimpleTypeWithOneParameter()
+        {
+            var container = new IocContainer();
+            container.Register<MySimpleThing>();
+            container.Register<MySlightlySimpleThing>();
+            var mySlightlySimpleThing = container.Resolve<MySlightlySimpleThing>();
+            mySlightlySimpleThing.Should().NotBeNull();
+            mySlightlySimpleThing.SimpleThing.Should().NotBeNull();
+            mySlightlySimpleThing.SimpleThing.MyName.Should().Be("PropSnippetYourself");
+        }
+
+        [Fact]
+        public void CanCreateSimpleTypeWithOneParameterUsingFactoryMethod()
+        {
+            var container = new IocContainer();
+            container.Register(() => new MySimpleThing()
+            {
+                MyName = "MyNameIsBob"
+            });
+            container.Register<MySlightlySimpleThing>();
+            var mySlightlySimpleThing = container.Resolve<MySlightlySimpleThing>();
+            mySlightlySimpleThing.Should().NotBeNull();
+            mySlightlySimpleThing.SimpleThing.Should().NotBeNull();
+            mySlightlySimpleThing.SimpleThing.MyName.Should().Be("MyNameIsBob");
+        }
+
+        [Fact]
+        public void CanCreateSimpleTypeWithOneParameterUsingActionMethod()
+        {
+            var container = new IocContainer();
+            container.Register<MySimpleThing>(t => t.MyName = "MyNameIsT");
+            container.Register<MySlightlySimpleThing>();
+            var mySlightlySimpleThing = container.Resolve<MySlightlySimpleThing>();
+            mySlightlySimpleThing.Should().NotBeNull();
+            mySlightlySimpleThing.SimpleThing.Should().NotBeNull();
+            mySlightlySimpleThing.SimpleThing.MyName.Should().Be("MyNameIsT");
+        }
+
+        [Fact]
+        public void CanRegisterInterfaceAndResolve()
+        {
+            var container = new IocContainer();
+
+            container.Register<IDatabaseRepository, SqlDatabaseRepository>();
+            container.Register<ZooController>();
+
+            var databaseRepository = container.Resolve<IDatabaseRepository>();
+            databaseRepository.GetType().Should().Be(typeof(SqlDatabaseRepository));
+
+            var controller = container.Resolve<ZooController>();
+
+            var result = controller.AddMonkeys();
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void CanRegisterInterfaceAndResolve_2()
+        {
+            var container = new IocContainer();
+
+            container.Register<IDatabaseRepository, MemoryDatabaseRepository>();
+            container.Register<ZooController>();
+
+            var databaseRepository = container.Resolve<IDatabaseRepository>();
+            databaseRepository.GetType().Should().Be(typeof(MemoryDatabaseRepository));
+
+            var controller = container.Resolve<ZooController>();
+
+            var result = controller.AddMonkeys();
+            result.Should().Be(10);
+        }
+
+        [Fact]
+        public void ResolvingATypeWithoutRegisteringAllDependenciesThrowsException()
+        {
+            var container = new IocContainer();
+
+            container.Register<ZooController>();
+
+            Action act = () =>
+            {
+                container.Resolve<ZooController>();
+            };
+
+            act.ShouldThrow<InvalidOperationException>().WithMessage("Unable to resolve constructor of type IocTests.IocTesting+ZooController");
+        }
+
+        [Fact]
+        public void ResolvingATypeWithoutRegisteringAllDependenciesForDependentThrowsException()
+        {
+            var container = new IocContainer();
+
+            container.Register<ZooController>();
+            container.Register<IDatabaseRepository, DependentDatabaseRepository>();
+
+            Action act = () =>
+            {
+                container.Resolve<ZooController>();
+            };
+
+            act.ShouldThrow<InvalidOperationException>().WithMessage("Unable to resolve constructor of type IocTests.IocTesting+DependentDatabaseRepository");
+        }
+
+        public class MySimpleThing
+        {
+            public string MyName { get; set; } = "PropSnippetYourself";
+        }
+
+        public class MySlightlySimpleThing
+        {
+
+            public MySlightlySimpleThing(MySimpleThing simpleThing)
+            {
+                SimpleThing = simpleThing;
+            }
+
+            public MySimpleThing SimpleThing { get; private set; }
+        }
+
+        public interface IDatabaseRepository
+        {
+            int AddMonkeys(string length);
+        }
+
+        public class SqlDatabaseRepository : IDatabaseRepository
+        {
+            public int AddMonkeys(string length)
+            {
+                return 1;
+            }
+        }
+
+        public class MemoryDatabaseRepository : IDatabaseRepository
+        {
+            public int AddMonkeys(string length)
+            {
+                return 10;
+            }
+        }
+
+        public class DependentDatabaseRepository : IDatabaseRepository
+        {
+            private MySimpleThing _mySimpleThing;
+
+            public DependentDatabaseRepository(MySimpleThing mySimpleThing)
+            {
+                _mySimpleThing = mySimpleThing;
+            }
+
+            public int AddMonkeys(string length)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class ZooController
+        {
+            private IDatabaseRepository _databaseRepository;
+
+            public ZooController(IDatabaseRepository databaseRepository)
+            {
+                _databaseRepository = databaseRepository;
+            }
+
+            public int AddMonkeys()
+            {
+                return _databaseRepository.AddMonkeys("ABC");
+            }
+        }
+    }
+}
